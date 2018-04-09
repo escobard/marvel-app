e4cee987f2c8f9f9f20ed88ced20c50f01428328
Daniel Escobar <escobard90@gmail.com>
2018-04-09 15:57:18 -0600

    feat: added dependencies to application, and import statements within app.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+import { BrowserRouter } from "react-router-dom";
+// required to work with async / await
+import "babel-polyfill";
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
