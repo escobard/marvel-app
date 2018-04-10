68e52c79f93a182626df5589b3fcf78571f10fc4
Daniel Escobar <escobard90@gmail.com>
2018-04-10 17:05:16 -0600

    feat: updated URI - focusing on infinity war characters only

--- src/constants/config.js
+++ src/constants/config.js
@@ -4,7 +4,7 @@
 const config = {
 	marvelPublic: 'c88aa66a7664cb7a2d8ec8f416c3922e',
 	marvelURL: 'http://gateway.marvel.com/v1/public/',
-	marvelEndpoint: 'comics?'
+	marvelEndpoint: 'events/29/characters?limit=99&'
 }
 
 export const marvelURI = `${config.marvelURL}${config.marvelEndpoint}apikey=${config.marvelPublic}`
